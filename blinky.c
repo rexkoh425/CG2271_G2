b6e71f85c5219bf98f67c8044f646f2ee3b4f0e5
JordanKwua <kwuachunren@gmail.com>
2024-11-05 11:49:24 +0800

    Add controller code to ESP32

--- blinky.c
+++ blinky.c
@@ -15,11 +15,104 @@
 #define BAUD_RATE 9600
 #define PTE23_Pin 23 //RX
 
-volatile uint8_t receive_data = 0;
 volatile uint8_t ISR_error = 0;
 volatile uint8_t running_state = 0;
+volatile uint8_t ending = 0;
+volatile uint8_t receiveddatafromESP = 0;
+volatile uint8_t receiveddataQueue = 0;
+volatile uint8_t received_data = 0;
+
+
 uint8_t isFrontLEDOn = 0;
 
+osThreadId_t esp_thread;
+
+
+// Define notes as frequencies in Hz
+#define NOTE_E5  659
+#define NOTE_D5  587
+#define NOTE_FS4 370
+#define NOTE_GS4 415
+#define NOTE_C5  523
+#define NOTE_B4  494
+#define NOTE_D4  294
+#define NOTE_E4  330
+
+// Define note durations in ms
+#define WHOLE_NOTE 500
+#define HALF_NOTE (WHOLE_NOTE / 2)
+#define QUARTER_NOTE (WHOLE_NOTE / 4)
+#define EIGHTH_NOTE (WHOLE_NOTE / 8)
+
+#define Q_SIZE (32)
+	typedef struct {
+	unsigned char Data[Q_SIZE];
+	unsigned int Head; // points to oldest data element
+	unsigned int Tail; // points to next free space
+	unsigned int Size; // quantity of elements in queue
+} Q_T;
+
+Q_T rx_q;
+
+void Q_Init(Q_T * q) {
+	unsigned int i;
+	for (i=0; i<Q_SIZE; i++)
+	q->Data[i] = 0; // to simplify our lives when debugging
+	q->Head = 0;
+	q->Tail = 0;
+	q->Size = 0;
+}
+int Q_Empty(Q_T * q) {
+	return q->Size == 0;
+}
+int Q_Full(Q_T * q) {
+	return q->Size == Q_SIZE;
+}
+
+int Q_Enqueue(Q_T * q, unsigned char d) {
+	// What if queue is full?
+	if (!Q_Full(q)) {
+	q->Data[q->Tail++] = d;
+	q->Tail %= Q_SIZE;
+	q->Size++;
+	return 1; // success
+	} else
+	return 0; // failure
+}
+unsigned char Q_Dequeue(Q_T * q) {
+	// Must check to see if queue is empty before dequeueing
+	unsigned char t=0;
+	if (!Q_Empty(q)) {
+	t = q->Data[q->Head];
+	q->Data[q->Head++] = 0; // to simplify debugging
+	q->Head %= Q_SIZE;
+	q->Size--;
+	}
+	return t;
+}
+
+
+void playEndingTone() {
+    setBuzzerFrequency(NOTE_E5); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_D5); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_FS4); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_GS4); osDelay(HALF_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+
+    setBuzzerFrequency(NOTE_C5); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_B4); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_D4); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_E4); osDelay(HALF_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+
+    setBuzzerFrequency(NOTE_E5); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_D5); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_FS4); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_GS4); osDelay(HALF_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+
+    setBuzzerFrequency(NOTE_C5); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_E4); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_B4); osDelay(QUARTER_NOTE); setBuzzerFrequency(0); osDelay(EIGHTH_NOTE);
+    setBuzzerFrequency(NOTE_GS4); osDelay(HALF_NOTE); setBuzzerFrequency(0); osDelay(HALF_NOTE);
+}
 
 void Init_UART2() {
 	uint32_t divisor,bus_clock;
@@ -52,8 +145,14 @@ void Init_UART2() {
 void UART2_IRQHandler(void) {
 	NVIC_ClearPendingIRQ(UART2_IRQn);
 	if (UART2->S1 & UART_S1_RDRF_MASK) {
-		receive_data = UART2->D;
+		//if (!Q_Full(&rx_q)) {
+	//	receiveddatafromESP = UART2->D; 
+		//	Q_Enqueue(&rx_q, UART2->D);
+	//	}
+		received_data = UART2->D;
+	//	osThreadFlagsSet(esp_thread,0x0001);
 	}
+	
 	if (UART2->S1 &  (UART_S1_OR_MASK |
 										UART_S1_NF_MASK |
 										UART_S1_FE_MASK |
@@ -72,43 +171,94 @@ static void delay(volatile uint32_t nof) {
   }
 }
 
+// 0x3n -> motor direction
+// 0x1n -> motor speed
+// 0x9n -> buzzer tone
 void ESP_response(uint8_t rx_data){
 	switch(rx_data) {
+		case 0x11:
+			motor_speed = 1;
+		  break;
+		case 0x12:
+			motor_speed = 0.5;
+		  break;
 		case 0x31:
 			running_state = 1;
+			ending = 0;
 			goLeft();
 		  break;
 		case 0x32:
 			running_state = 1;
+			ending = 0;
 			goRight();
 		  break;
 		case 0x33:
 			running_state = 1;
+			ending = 0;
 			goForward();
 		  break;
 		case 0x34:
 			running_state = 1;
+			ending = 0;
 			goBackward();
 		  break;
+		case 0x35:
+			running_state = 1;
+			ending = 0;
+			moveLeftForward();
+		  break;
+		case 0x36:
+			running_state = 1;
+			ending = 0;
+			moveRightForward();
+		  break;
+		case 0x37:
+			running_state = 1;
+			ending = 0;
+			moveLeftBackward();
+		  break;
+		case 0x38:
+			running_state = 1;
+			ending = 0;
+			moveRightBackward();
+		  break;
+		case 0x98:
+			ending = 0;
+			break;
+		case 0x99:
+			ending = 1;
+	    break;
 		default:
 			running_state = 0;
 			stop();
+			ending = 0;
 		  break;
 	}
 }
 
 void ESPresponse(void *argument) {
 	for(;;) {
-		ESP_response(receive_data);
+		
+	//	if(!Q_Empty(&rx_q)){
+		//	receiveddataQueue = Q_Dequeue(&rx_q);
+		//  osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
+			ESP_response(received_data);
+	//	} else if (Q_Full(&rx_q)){
+		//ESP_response(0xFF);
+	//	}
 	}
 }
 
 void playMusic(void *argument){
     for(;;){
-        setBuzzerFrequency(WEE_FREQUENCY);
-        osDelay(500);
-        setBuzzerFrequency(WOO_FREQUENCY);
-        osDelay(500);
+				if(ending == 0){
+					setBuzzerFrequency(WEE_FREQUENCY);
+					osDelay(500);
+					setBuzzerFrequency(WOO_FREQUENCY);
+					osDelay(500);
+				}else{
+					playEndingTone();
+				}
     }
 }
 
@@ -117,34 +267,42 @@ void playMusic(void *argument){
 void ledFront(void *argument){
     for(;;){
         if(running_state == 1){
-			isFrontLEDOn = 1;
+						isFrontLEDOn = 0;
+						PTE->PDOR &= ~MASK(PTE2_Pin);
+						PTE->PDOR &= ~MASK(PTE3_Pin);
+						PTE->PDOR &= ~MASK(PTE4_Pin);
+						PTE->PDOR &= ~MASK(PTE5_Pin);
+						PTB->PDOR &= ~MASK(PTB8_Pin);
+						PTB->PDOR &= ~MASK(PTB9_Pin);
+						PTB->PDOR &= ~MASK(PTB10_Pin);
+						PTB->PDOR &= ~MASK(PTB11_Pin);
+					
             PTE->PDOR |= MASK(PTE5_Pin);
-            PTE->PDOR &= ~MASK(PTE5_Pin);
-            osDelay(100);
+            osDelay(200);
+						PTE->PDOR &= ~MASK(PTE5_Pin);
             PTE->PDOR |= MASK(PTE4_Pin);
-            PTE->PDOR &= ~MASK(PTE4_Pin);
-            osDelay(100);
+            osDelay(200);
+						PTE->PDOR &= ~MASK(PTE4_Pin);
             PTE->PDOR |= MASK(PTE3_Pin);
-            PTE->PDOR &= ~MASK(PTE3_Pin);
-            osDelay(100);
+            osDelay(200);
+						PTE->PDOR &= ~MASK(PTE3_Pin);
             PTE->PDOR |= MASK(PTE2_Pin);
-            PTE->PDOR &= ~MASK(PTE2_Pin);
-            osDelay(100);
+            osDelay(200);
+						PTE->PDOR &= ~MASK(PTE2_Pin);
             PTB->PDOR |= MASK(PTB11_Pin);
-            PTB->PDOR &= ~MASK(PTB11_Pin);
-            osDelay(100);
+            osDelay(200);
+					  PTB->PDOR &= ~MASK(PTB11_Pin);
             PTB->PDOR |= MASK(PTB10_Pin);
-            PTB->PDOR &= ~MASK(PTB10_Pin);
-            osDelay(100);
+            osDelay(200);
+						PTB->PDOR &= ~MASK(PTB10_Pin);
             PTB->PDOR |= MASK(PTB9_Pin);
-            PTB->PDOR &= ~MASK(PTB9_Pin);
-            osDelay(100);
+            osDelay(200);
+						PTB->PDOR &= ~MASK(PTB9_Pin);
             PTB->PDOR |= MASK(PTB8_Pin);
-            PTB->PDOR &= ~MASK(PTB8_Pin);
-            osDelay(100);
-        } else {
-			if(!isFrontLEDOn){
-				PTE->PDOR |= MASK(PTE5_Pin);
+            osDelay(200);
+						PTB->PDOR &= ~MASK(PTB8_Pin);
+        } else if(!isFrontLEDOn){
+							PTE->PDOR |= MASK(PTE5_Pin);
             	PTE->PDOR |= MASK(PTE4_Pin);
             	PTE->PDOR |= MASK(PTE3_Pin);
             	PTE->PDOR |= MASK(PTE2_Pin);
@@ -152,9 +310,8 @@ void ledFront(void *argument){
             	PTB->PDOR |= MASK(PTB10_Pin);
             	PTB->PDOR |= MASK(PTB9_Pin);
             	PTB->PDOR |= MASK(PTB8_Pin);
-				isFrontLEDOn = 1;
+							isFrontLEDOn = 1;
 			}
-        }
     }
 }
 
@@ -186,7 +343,7 @@ int main()
 	
 	
 	osKernelInitialize();	// Initialize CMSIS-RTOS
-  	//osThreadNew(ledOn , NULL, NULL);    // Create application main thread
+ // osThreadNew(ledOn , NULL, NULL);    // Create application main thread
 	osThreadNew(ESPresponse, NULL, NULL); // Thread to run motors
 	osThreadNew(playMusic, NULL, NULL); // Thread to play music
 	osThreadNew(ledFront, NULL, NULL); // Thread to run front LEDs
---
+++ esp32/PhoneController.ino
@@ -0,0 +1,121 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+//#define USE_PIN // Uncomment this to use PIN during pairing. The pin is specified on the line below
+const char *pin = "1234"; // Change this to more secure PIN.
+
+String device_name = "ESP32-BT-Slave";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+#define RXD2 16
+#define TXD2 17
+
+BluetoothSerial SerialBT;
+String message = "";
+String response;
+char incomingChar;
+
+void respondWith(String response) {
+  response = response.c_str();
+  for (int i = 0; response[i] != '\0';i++) {
+    SerialBT.write(response[i]);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  SerialBT.begin(device_name); //Bluetooth device name
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  //Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
+  #ifdef USE_PIN
+    SerialBT.setPin(pin);
+    Serial.println("Using PIN");
+  #endif
+}
+
+void loop() {
+  // if (Serial.available()) {
+  //   SerialBT.write(Serial.read());
+  // }
+  if (SerialBT.available()){
+    char incomingChar = SerialBT.read();
+    if (incomingChar != '\n'){
+      message += String(incomingChar);
+    }
+    else{
+      message = "";
+    }
+    Serial.write(incomingChar);  
+  }
+  // Check received message and control output accordingly
+  if (message =="status"){
+  }
+  else if (message =="4"){
+    response = "TURNING LEFT";
+    Serial2.write(0x31);
+  }
+  else if (message == "2") {
+    response = "TURNING RIGHT";
+    Serial2.write(0x32);
+  }
+  else if (message == "1") {
+    response = "MOVING FORWARD";
+    Serial2.write(0x33);
+  }
+  else if (message == "3") {
+    response = "MOVING BACKWARD";
+    Serial2.write(0x34);
+  }
+  else if (message == "8") {
+    response = "MOVING LEFT FORWARD";
+    Serial2.write(0x35);
+  }
+  else if (message == "5") {
+    response = "MOVING RIGHT FORWARD";
+    Serial2.write(0x36);
+  }
+  else if (message == "7") {
+    response = "MOVING LEFT BACKWARD";
+    Serial2.write(0x37);
+  }
+  else if (message == "6") {
+    response = "MOVING RIGHT BACKWARD";
+    Serial2.write(0x38);
+  }
+  else if (message == "0") {
+    response = "STOPPING";
+    Serial2.write(0x39);
+  }
+  else if (message == "f") {
+    response = "FULL SPEED";
+    Serial2.write(0x11);
+  }
+  else if (message == "h") {
+    response = "HALF SPEED";
+    Serial2.write(0x12);
+  }
+  else if (message == "e") {
+    response = "PLAY END TONE";
+    Serial2.write(0x99);
+  }
+  else if (message == "se") {
+    response = "STOP TONE";
+    Serial2.write(0x98);
+  }
+  respondWith(response);
+  message = "";
+  delay(20);
+}
